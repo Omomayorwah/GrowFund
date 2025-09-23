f50f9be07b99698e98c833e2f6c0787cbae9e96b
Omomayorwah <kintunde2010@gmail.com>
2025-09-23 04:46:31 +0100

    updated frontend connection

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL =import.meta.env.REACT_APP_API_URL || 'http://localhost:5000';
+export const API_BASE_URL =import.meta.env.REACT_APP_API_URL || 'https://growfund-tjpz.onrender.com';
